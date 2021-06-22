94060844c1cee87c78a979936a6912c5ec423a01
TychoVI <tycho@vanisterdael.email>
2021-06-22 12:18:45 +0200

    finally finally fix cors

--- MenuApp.ApiGateway/Startup.cs
+++ MenuApp.ApiGateway/Startup.cs
@@ -27,6 +27,7 @@ namespace MenuApp.ApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddOcelot(Configuration);
         }
 
